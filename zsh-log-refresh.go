9b1a19ae2880238b86d2beaf9a44d7ff2377476b
nnao45 <n4sekai5y@gmail.com>
2018-05-25 19:20:09 +0900

    zsh-loggin imple

--- zsh-log-refresh.go
+++ zsh-log-refresh.go
@@ -9,17 +9,21 @@ import (
 	"sort"
 	"strings"
 
+	"github.com/codeskyblue/go-sh"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var version string
 
 var (
-	app            = kingpin.New("zsh-log-refresh", "A zsh-log-refresh application.")
-	limit          = app.Flag("l", "log refresh limit").Default("500").Int()
-	prefix         = app.Flag("s", "search log name").Required().String()
-	logDir         = app.Flag("s", "term log dir").Required().String()
-	runtimeLogFile = app.Flag("s", "this script runtime logging file").Default("./").String()
+	app    = kingpin.New("zsh-log-refresh", "A zsh-log-refresh application.")
+	limit  = app.Flag("l", "log refresh limit").Default("500").Int()
+	prefix = app.Flag("s", "search log name").Required().String()
+)
+
+const (
+	LOG_DIR         = "~/Documents/term_logs"
+	RUNTIME_LOGFILE = "goruntime_err.log"
 )
 
 func addog(text string, filename string) {
@@ -36,34 +40,18 @@ func addog(text string, filename string) {
 	defer writeFile.Close()
 }
 
-func main() {
-	app.HelpFlag.Short('h')
-	app.Version(fmt.Sprint("zsh-log-refresh's version: ", version))
-	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
-	//
+func zshLogger() {
+	if os.Getenv("TERM") == "screen" || os.Getenv("TERM") == "screen-256color" {
+		sh.Command("tmux", "set-option", "default-terminal", "\"screen\"").Run()
+		sh.Command("pipe-pane", "cat", ">>", "$LOGDIR/$LOGFILE").Run()
+		sh.Command("display-message", "💾Started logging to $LOGDIR/$LOGFILE").Run()
 	}
-
-	var files = dirwalk(*logDir)
-
-	sort.Slice(files, func(i, j int) bool {
-		return files[i] > files[j]
-	})
-
-	for i, file := range files {
-		if i > *limit {
-			if err := os.RemoveAll(file); err != nil {
-				addog(fmt.Sprint(err), *runtimeLogFile)
-			}
-		}
-	}
-
-	fmt.Println("\n", "Log refresh is done.")
 }
 
 func dirwalk(dir string) []string {
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
-		addog(fmt.Sprint(err), *runtimeLogFile)
+		addog(fmt.Sprint(err), RUNTIME_LOGFILE)
 	}
 
 	var paths []string
@@ -78,4 +66,39 @@ func dirwalk(dir string) []string {
 	}
 
 	return paths
+}
+
+func init() {
+	var err error
+	if _, err = os.Stat(LOG_DIR); err != nil {
+		if err := os.MkdirAll(LOG_DIR, 0700); err != nil {
+			panic(err)
+		}
+	}
+
+	zshLogger()
+}
+
+func main() {
+	app.HelpFlag.Short('h')
+	app.Version(fmt.Sprint("zsh-log-refresh's version: ", version))
+	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+	//
+	}
+
+	var files = dirwalk(LOG_DIR)
+
+	sort.Slice(files, func(i, j int) bool {
+		return files[i] > files[j]
+	})
+
+	for i, file := range files {
+		if i > *limit {
+			if err := os.RemoveAll(file); err != nil {
+				addog(fmt.Sprint(err), RUNTIME_LOGFILE)
+			}
+		}
+	}
+
+	fmt.Println("\n", "Log refresh is done.")
 }
